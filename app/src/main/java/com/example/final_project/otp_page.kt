69d5bc736746e8511ffc03774e94b3f0c81e129e
macos <senghuot106@gmail.com>
2025-07-23 13:27:13 -0700

    connect database

--- app/src/main/java/com/example/final_project/otp_page.kt
+++ app/src/main/java/com/example/final_project/otp_page.kt
@@ -1,20 +1,33 @@
 package com.example.final_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.final_project.databinding.ActivityMainBinding
+import com.example.final_project.databinding.ActivityOtpPageBinding
 
 class otp_page : AppCompatActivity() {
+    lateinit var binding: ActivityOtpPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityOtpPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_otp_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val gotohome = findViewById<Button>(R.id.btnSubmit)
+        gotohome.setOnClickListener {
+            val intent = Intent(this, home_page::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/final_project/reset_password.kt
+++ app/src/main/java/com/example/final_project/reset_password.kt
@@ -1,20 +1,31 @@
 package com.example.final_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.final_project.databinding.ActivityLoginPageBinding
 
 class reset_password : AppCompatActivity() {
+    lateinit var binding: ActivityLoginPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
         setContentView(R.layout.activity_reset_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val gobacktologin = findViewById<TextView>(R.id.gobacktologin)
+        gobacktologin.setOnClickListener {
+            val intent = Intent(this, login_page::class.java)
+            startActivity(intent)
         }
     }
 }
